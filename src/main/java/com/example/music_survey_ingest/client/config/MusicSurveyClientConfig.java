48403ebed510b5157832ed1ba64a5cad590d40f2
Tobias Redeker <tobias.redeker@bit.admin.ch>
2025-06-08 15:01:41 +0200

    Refactor MusicSurveyClientConfig

--- src/main/java/com/example/music_survey_ingest/client/config/MusicSurveyClientConfig.java
+++ src/main/java/com/example/music_survey_ingest/client/config/MusicSurveyClientConfig.java
@@ -1,22 +1,18 @@
-package com.example.music_survey_ingest.config;
+package com.example.music_survey_ingest.client.config;
 
 import com.example.music_survey_ingest.client.MusicSurveyClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-import org.springframework.web.service.invoker.RestClientAdapter;
 
 @Configuration
 public class MusicSurveyClientConfig {
-
-    @Value("${client.music-survey.base-url}")
-    private String baseUrl;
-
     @Bean
-    MusicSurveyClient musicSurveyClient(RestClient.Builder builder) {
-        RestClient restClient = builder.baseUrl(baseUrl).build();
+    MusicSurveyClient musicSurveyClient(@Value("${client.music-survey.base-url}") String baseUrl) {
+        RestClient restClient = RestClient.builder().baseUrl(baseUrl).build();
         HttpServiceProxyFactory factory =
                 HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
         return factory.createClient(MusicSurveyClient.class);
